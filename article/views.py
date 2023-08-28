f7517fd6c3d7188374281ce441a7c65f09dc9fb0
wyewyewye <1083183486@qq.com>
2023-08-29 01:57:22 +0800

    [add] add bootstrap5 and base html

--- article/views.py
+++ article/views.py
@@ -7,7 +7,7 @@ def article_list(request):
     articles = ArticlePost.objects.all()
     print(articles)
     context = {'articles': articles}
-    return render(request, 'article/article_list.html', context)
+    return render(request, 'article/list.html', context)
 
 def test404(request):
     return HttpResponseNotFound()

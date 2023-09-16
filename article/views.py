79e35401f7aca53d8d4192592dcc1e6d36919082
wyewyewye <1083183486@qq.com>
2023-09-16 15:29:56 +0800

    [add] add create article

--- article/views.py
+++ article/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse, HttpResponseNotFound
 from .models import ArticlePost
+from .form import ArticlePostForm
+from django.contrib.auth.models import User
+
+import markdown
 
 def print_request(req):
     print(f"""
@@ -19,14 +23,37 @@ def article_list(request):
     context = {'articles': articles}
     return render(request, 'article/list.html', context)
 
-@require_http_methods(['GET'])
+@require_http_methods(['GET', 'POST'])
 def article_detail(request, id):
     print_request(request)
     if id <= 0 or id > ArticlePost.objects.count():
         return HttpResponseNotFound()
     article = ArticlePost.objects.get(id=id)
+    text = article.body
+    article.body = markdown.markdown(text,
+        extensions=[
+            'markdown.extensions.extra',
+            'markdown.extensions.codehilite',
+        ])
     context = {'article': article}
     return render(request, 'article/detail.html', context)
+
+@require_http_methods(['GET', 'POST'])
+def article_create(request):
+    print_request(request)
+    if request.method == 'POST':
+        article_post_form = ArticlePostForm(data=request.POST)
+        if article_post_form.is_valid():
+            new_article = article_post_form.save(commit=False)
+            new_article.author = User.objects.get(id=1)
+            new_article.save()
+            return redirect('article:article_list')
+        else:
+            return HttpResponse('表单内容有误，请重新填写')
+    else:
+        article_post_form = ArticlePostForm()
+        context = {'article_post_form': article_post_form}
+        return render(request, 'article/create.html', context)
 
 def test404(request):
     return HttpResponseNotFound()
